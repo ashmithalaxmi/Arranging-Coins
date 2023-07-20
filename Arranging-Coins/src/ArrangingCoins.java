36526440456810b529308530b6d53e5a0bf0c656
ashmithalaxmi <88831061+ashmithalaxmi@users.noreply.github.com>
2023-07-20 09:07:47 +0530

    Add files via upload

---
+++ Arranging-Coins/src/ArrangingCoins.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class ArrangingCoins {
+    public static int arrange(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        if (n <= 3) {
+            return n == 3 ? 2 : 1;
+        }
+        long start = 2;
+        long end = n / 2;
+        while (start <= end) {
+            long mid = start + (end - start) / 2;
+            long coinsFilled = mid * (mid + 1) / 2;
+            if (coinsFilled == n) {
+                return (int) mid;
+            }
+            if (coinsFilled < n) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return(int) end;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        System.out.println(arrange(n));
+    }
+}
